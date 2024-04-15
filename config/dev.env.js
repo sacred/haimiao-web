28f4eb82e0d80e3516dd3e6bd28e16a04eb1d199
sacred <sacred02@163.com>
2024-04-15 23:55:30 +0800

    生产url

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"https://api.egc168.com"'
+  BASE_API: '"http://localhost:8080"'
 })
